366fe1fdb0d7e6becc36647ffbf0da45f9f6c736
akandil98 <a.kandil.se@gmail.com>
2022-06-14 18:42:28 +0200

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,31 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = 'Tap the scren';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: Text(title),
       ),
+      body: GestureDetector(
+          onTap: () {
+            setState(() {
+              title = DateTime.now().toIso8601String();
+            });
+          },
+          child: Container(
+            color: Colors.white,
+          )),
     );
   }
 }
